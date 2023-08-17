45f2b7bcd09ddf72ed2e79760aa0702b7915e0b8
Sayaka Vincent <sfv2010@hotmail.fr>
2023-08-17 23:53:39 +0200

    Initial commit

---
+++ frontend/src/pages/Page404/ErrorAPI.jsx
@@ -0,0 +1,10 @@
+import "./ErrorApi.css";
+function ErrorAPI() {
+    return (
+        <p className="errorContainer errorApi">
+            Désolé, Impossible de charger des donées...😢
+        </p>
+    );
+}
+
+export default ErrorAPI;
